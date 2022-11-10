ae583f8eef008beda2dd2d5e76389d70a63fd274
Jack English <51383165+AUT0M4T1C-JACK@users.noreply.github.com>
2022-11-09 21:38:04 -0500

    Color vectors with parameters!
    As a demo, a yellow vector is spawned initially and upon left trigger press any other added vector is magenta.

--- Assets/Scripts/VectorController.cs
+++ Assets/Scripts/VectorController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -23,6 +25,8 @@ public class VectorController : MonoBehaviour
     {
         m_VectorBuffer = new List<GameObject>();
         toggleReference.action.started += Trigger;
+        SpawnVector(Vector3.one, Color.yellow);
+        
     }
 
     // Update is called once per frame
@@ -66,12 +70,21 @@ public class VectorController : MonoBehaviour
         return m_VectorBuffer[index].GetComponent<VectorRenderer>().vector;
     }
 
+    // When left trigger is pressed (defined in the script inspector), a default vector is spawned
     private void Trigger(InputAction.CallbackContext context) 
     {
-        SpawnVector();
+        SpawnVector(new Vector3(1, 2, 3), Color.magenta);
     }
 
-    public void SpawnVector()
+    public void ChangeColor(int vectorIndex, Color color)
+    {
+        var material = m_VectorBuffer[vectorIndex].GetComponent<VectorRenderer>().GetMaterial();
+        material.SetColor("_EmissionColor", color);
+        material.SetColor("_BaseColor", color);
+        m_VectorBuffer[vectorIndex].GetComponent<VectorRenderer>().SetMaterial(material);
+    }
+
+    public void SpawnVector(Vector3 vector, Color color)
     {
         GameObject vectorCylinder = Resources.Load("VectorCylinder") as GameObject;
         GameObject instance = new GameObject($"Vector{m_VectorBuffer.Count}");
@@ -79,7 +92,10 @@ public class VectorController : MonoBehaviour
         instance.transform.parent = transform;
         instance.transform.localScale = Vector3.one;
         instance.transform.localPosition = Vector3.zero;
+        instance.SetActive(true);
+        instance.GetComponent<VectorRenderer>().vector = vector;
 
         m_VectorBuffer.Add(instance);
+        ChangeColor(m_VectorBuffer.Count - 1, color);
     }
 }
